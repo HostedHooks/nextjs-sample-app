d19103752231abe89a7766d6433ccef637ed6f76
salimabsi0 <salimabsifull@gmail.com>
2021-08-09 15:07:35 +0300

    Add comments

--- utils/hostedhooks.js
+++ utils/hostedhooks.js
@@ -1,38 +1,35 @@
 export default function sendWebhookMessage(event, todo) {
-
   var url = new URL(`https://www.hostedhooks.com/api/v1/apps/${process.env.NEXT_PUBLIC_APP_UUID}/messages`);
 
   var myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${process.env.NEXT_PUBLIC_HOSTEDHOOKS_API_KEY}`);
-  myHeaders.append("Content-Type", "application/json");
-
+  myHeaders.append('Authorization', `Bearer ${process.env.NEXT_PUBLIC_HOSTEDHOOKS_API_KEY}`);
+  myHeaders.append('Content-Type', 'application/json');
 
   // ex: user session data
   const user = {
     id: 'user_id',
     role: 'team_manager',
-  }
+  };
 
-  // Build message payload
+  // webhook message
   var messagePayload = JSON.stringify({
-    "data": {
-      "user": user,
-      "todo": todo,
+    data: {
+      user: user,
+      todo: todo, // todo data
     },
-    "version": "1.0",
-    "event_type": event
+    version: '1.0',
+    event_type: event,
   });
 
   var requestOptions = {
     method: 'POST',
     headers: myHeaders,
     body: messagePayload,
-    redirect: 'follow'
+    redirect: 'follow',
   };
 
   fetch(url, requestOptions)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.error(error));
-
+    .then((response) => response.text())
+    .then((result) => console.log(result))
+    .catch((error) => console.error(error));
 }
